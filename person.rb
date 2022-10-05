c216fed29b72dcc024e64f179d9be8aa118a9eaf
bengigenc <genc.bengi@gmail.com>
2022-10-05 13:30:39 +0300

    Turn person class to nameable by inheritance

--- person.rb
+++ person.rb
@@ -1,9 +1,16 @@
-class Person
+require_relative 'nameble'
+
+class Person < Nameable
   def initialize(age, name = 'Unkown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
+  end
+
+  def correct_name
+    @name
   end
 
   def of_age?
@@ -18,6 +25,8 @@ class Person
   def can_use_services?
     is_of_age? || parent_permission
   end
+
+
 
   attr_accessor :name, :age
   attr_reader :id
