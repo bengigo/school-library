9ec6687b6b25969e0edaf8cf0bf9098bbae5b12a
bengigenc <genc.bengi@gmail.com>
2022-10-05 18:57:20 +0300

    Add class relationships

--- rental.rb
+++ rental.rb
@@ -1,7 +1,13 @@
-class Rental
-  attr_accessor :date
+require_relative 'book'
 
-  def initialize(date)
+class Rental
+  attr_accessor :date, :book, :person
+
+  def initialize(date, book, person)
     @date = date
+    @book = book
+    book.rentals << self
+    @person = person
+    person.rentals << self
   end
 end
--- student.rb
+++ student.rb
@@ -1,9 +1,18 @@
 require './person'
+require_relative 'classroom'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unkown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless
+    classroom.students.include?(self)
   end
 
   def play_hooky
